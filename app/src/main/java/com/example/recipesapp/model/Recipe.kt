f39ae8125ba97783edeeb3a24b511de869deaeca
a.kizik <a.v.kizik@gmail.com>
2025-09-26 11:29:16 +0300

    RA-24

--- app/src/main/java/com/example/recipesapp/model/Recipe.kt
+++ app/src/main/java/com/example/recipesapp/model/Recipe.kt
@@ -7,6 +7,7 @@ import kotlinx.parcelize.Parcelize
 data class Recipe(
     val id: Int,
     val title: String,
+    val servings: Int = 1,
     val ingredients: List<Ingredient>,
     val method: List<String>,
     val imageUrl: String,
