3a3d54bf1b84bee285e23200f3220e477e5ebde0
anna_kizik <a.v.kizik@gmail.com>
2025-10-19 18:58:37 +0300

    RA-30

--- app/src/main/java/com/example/recipesapp/ui/recipes/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/recipesapp/ui/recipes/favorites/FavoritesFragment.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import com.example.recipesapp.R
 import com.example.recipesapp.databinding.FragmentFavoritesBinding
-import com.example.recipesapp.model.ARG_RECIPE_ID
 import com.example.recipesapp.ui.recipes.recipeslist.RecipesListAdapter
 import java.lang.IllegalStateException
 
@@ -64,11 +61,11 @@ class FavoritesFragment : Fragment() {
     }
 
     private fun openRecipeByRecipeId(recipeId: Int) {
-        val bundle = bundleOf(
-            ARG_RECIPE_ID to recipeId
+        findNavController().navigate(
+            FavoritesFragmentDirections.actionFavoritesFragmentToRecipeFragment(
+                recipeId
+            )
         )
-
-        findNavController().navigate(R.id.recipeFragment, bundle)
     }
 
     override fun onDestroyView() {
