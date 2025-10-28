a40425a81977df62feea619adbdde5f1a73561bd
anna_kizik <a.v.kizik@gmail.com>
2025-10-29 00:16:15 +0300

    RA-35

--- app/src/main/java/com/example/recipesapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/recipesapp/ui/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.recipesapp.ui
 
-import android.adservices.adselection.GetAdSelectionDataRequest
 import android.os.Bundle
-import android.telecom.Call
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,11 +11,12 @@ import com.example.recipesapp.R
 import com.example.recipesapp.RecipeApiService
 import com.example.recipesapp.databinding.ActivityMainBinding
 import com.example.recipesapp.model.Category
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Call
 import retrofit2.Retrofit
-import java.net.HttpURLConnection
-import java.net.URL
-import java.util.concurrent.Executors
+import retrofit2.Retrofit.Builder
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
@@ -25,7 +24,6 @@ class MainActivity : AppCompatActivity() {
         get() = _binding ?: throw IllegalStateException(
             "Binding for ActivityMainBinding must not be null"
         )
-    private val threadPool = Executors.newFixedThreadPool(10)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,43 +32,19 @@ class MainActivity : AppCompatActivity() {
 
         val thread = Thread {
 
+            val contentType = "application/json".toMediaType()
             val retrofit: Retrofit = Builder()
                 .baseUrl("https://recipes.androidsprint.ru/api/")
+                .addConverterFactory(Json.asConverterFactory(contentType))
                 .build()
 
             val service: RecipeApiService = retrofit.create(RecipeApiService::class.java)
 
             val categoriesCall: Call<List<Category>> = service.getCategories()
             val categoriesResponse = categoriesCall.execute()
+            val categories = categoriesResponse.body()
 
-
-            val url = URL("https://recipes.androidsprint.ru/api/category")
-            val connection = url.openConnection() as HttpURLConnection
-            connection.connect()
-            val body = connection.inputStream.bufferedReader().readText()
-            val json = Json {
-                ignoreUnknownKeys = true
-            }
-
-            Log.i("!!!", "responseCode: ${connection.responseCode}")
-            Log.i("!!!", "responseMessage: ${connection.responseMessage}")
-            Log.i("!!!", "Body: $body")
-            Log.i("Thread", "Выполняю запрос на потоке: ${Thread.currentThread().name}")
-
-            val categories = json.decodeFromString<List<Category>>(body)
-            val categoriesIds = categories.map { it.id }
-            categoriesIds.forEach { categoryId ->
-                threadPool.execute {
-                    val recipesByCategoryId =
-                        URL("https://recipes.androidsprint.ru/api/category/${categoryId}/recipes")
-                    val recipesConnection =
-                        recipesByCategoryId.openConnection() as HttpURLConnection
-                    recipesConnection.connect()
-                    val recipesBody = recipesConnection.inputStream.bufferedReader().readText()
-                    Log.d("Recipes", "Category $categoryId recipes: $recipesBody")
-                }
-            }
-            connection.disconnect()
+            Log.i("!!!", "categories: ${categories.toString()}")
         }
         thread.start()
         Log.i("Thread", "Метод onCreate() выполняется на потоке: ${Thread.currentThread().name}")
@@ -91,4 +65,5 @@ class MainActivity : AppCompatActivity() {
             findNavController(R.id.nav_host_fragment).navigate(R.id.favoritesFragment)
         }
     }
+
 }
--- app/src/main/java/com/example/recipesapp/ui/recipes/recipeslist/RecipesListViewModel.kt
+++ app/src/main/java/com/example/recipesapp/ui/recipes/recipeslist/RecipesListViewModel.kt
@@ -5,15 +5,18 @@ import android.app.Application
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.example.recipesapp.data.STUB.getRecipesByCategoryId
+import com.example.recipesapp.RecipesRepository
+import com.example.recipesapp.ThreadPool
 import com.example.recipesapp.model.Category
 import com.example.recipesapp.model.Recipe
 
-
 class RecipesListViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository = RecipesRepository(ThreadPool.threadPool)
 
     private val _recipesListState = MutableLiveData<RecipesListState>()
     val recipesListState: LiveData<RecipesListState> get() = _recipesListState
@@ -28,7 +31,6 @@ class RecipesListViewModel(application: Application) : AndroidViewModel(applicat
     )
 
     fun loadRecipesListForCategory(category: Category) {
-
         val categoryImage = try {
             val imagePath = category.imageUrl
             if (imagePath.isNotEmpty()) {
@@ -40,13 +42,18 @@ class RecipesListViewModel(application: Application) : AndroidViewModel(applicat
             Log.e("CategoryLoad", "Ошибка при загрузке изображения категории: ${e.message}", e)
             null
         }
-        val recipesList = getRecipesByCategoryId(category.id)
+        repository.loadRecipesByCategoryId(category.id) { recipesList ->
+            if (recipesList == null) Toast.makeText(
+                context,
+                "Ошибка получения данных",
+                Toast.LENGTH_SHORT
+            ).show()
 
-        _recipesListState.value = RecipesListState(
-            category = category,
-            categoryImage = categoryImage,
-            recipesList = recipesList
-        )
-
+            _recipesListState.value = RecipesListState(
+                category = category,
+                categoryImage = categoryImage,
+                recipesList = recipesList ?: emptyList()
+            )
+        }
     }
 }
